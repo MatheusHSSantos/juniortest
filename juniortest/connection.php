d1c8a09c46429a802674e2ccae7268dc42e022ac
MatheusHSSantos <115360147+MatheusHSSantos@users.noreply.github.com>
2023-02-14 15:04:54 -0300

    Add files via upload

---
+++ juniortest/connection.php
@@ -0,0 +1,9 @@
+<?php
+
+$host = "localhost";
+$user = "id20164123_product";
+$pass = "is]engVeJftd0+GI";
+$dbname = "id20164123_products";
+$port = "3306";
+
+$conn = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname, $user, $pass);
