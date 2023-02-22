f8bdd83a4e28663361e01586698d81da93690d0b
MatheusHSSantos <115360147+MatheusHSSantos@users.noreply.github.com>
2023-02-22 11:31:31 -0300

    Delete juniortest directory

--- juniortest/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$host = "localhost";
-$user = "id20164123_product";
-$pass = "is]engVeJftd0+GI";
-$dbname = "id20164123_products";
-$port = "3306";
-
-$conn = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname, $user, $pass);
